e06c09c896b878a8bd52bc3b85399307e5c5bb70
pangPython <pangpython@qq.com>
2017-05-15 07:43:54 +0800

    add jars

---
+++ src/cn/edu/bzu/zw/mytally/utils/MyPOI.java
@@ -0,0 +1,25 @@
+package cn.edu.bzu.zw.mytally.utils;
+
+import java.util.List;
+
+import cn.edu.bzu.zw.mytally.main.MyTally;
+
+/**
+ * @Project MyTally
+ * @Package cn.edu.bzu.zw.mytally.utils
+ * @Author ZhangWei
+ * @Time 上午7:34:50
+ */
+public class MyPOI {
+
+	/**
+	 * 使用apache poi jar包 把列表导出为excel
+	 * @param list
+	 * @return
+	 */
+	
+	public static String exportList2Excel(List<MyTally> list){
+		
+		return "";
+	}
+}
